09322ebbec708e83caa9086158cfa600addf27ad
yobibyte <y0b1byte@proton.me>
2024-10-27 08:02:14 +0000

    cleanup

--- dotfiles/init.lua
+++ dotfiles/init.lua
@@ -15,13 +15,15 @@ vim.o.completeopt = 'menuone,noselect'
 vim.o.termguicolors = true
 vim.wo.relativenumber = true
 vim.o.autoread = true -- update buffers if files were changed outside
+
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system {'git', 'clone', '--filter=blob:none', 'https://github.com/folke/lazy.nvim.git', '--branch=stable', lazypath,} end
+  vim.fn.system {'git', 'clone', '--filter=blob:none', 'https://github.com/folke/lazy.nvim.git', '--branch=stable', lazypath,} 
+end
 vim.opt.rtp:prepend(lazypath)
+
 require('lazy').setup({
   'tpope/vim-fugitive',
-  'simrat39/rust-tools.nvim',
   'tpope/vim-sleuth',
   'mbbill/undotree',
   {'stevearc/aerial.nvim',opts = {},dependencies = {"nvim-treesitter/nvim-treesitter",},},
@@ -32,12 +34,13 @@ require('lazy').setup({
   {'nvim-telescope/telescope.nvim', defaults={file_ignore_patterns={".venv.",},}, branch = '0.1.x', dependencies = { 'nvim-lua/plenary.nvim',
     {'nvim-telescope/telescope-fzf-native.nvim', build = 'make', cond = function() return vim.fn.executable 'make' == 1 end,},},},
   {'nvim-treesitter/nvim-treesitter', dependencies = {'nvim-treesitter/nvim-treesitter-textobjects',},build = ':TSUpdate',},
-  {"danymat/neogen", dependencies = "nvim-treesitter/nvim-treesitter", config = true,
-    languages = { python = { template = { annotation_convention = "google_docstrings" } } },}}, {})
-vim.api.nvim_create_autocmd('TextYankPost', {callback = function() vim.highlight.on_yank() end,
-  group = vim.api.nvim_create_augroup('YankHighlight', {clear = true }), pattern = '*',})
-require('telescope').setup {defaults = {mappings = {i = {['<C-u>'] = false,['<C-d>'] = false,},},},}
+  {"danymat/neogen", dependencies = "nvim-treesitter/nvim-treesitter", config = true, languages = { python = { template = { annotation_convention = "google_docstrings" } } },}}, {}
+)
+
+vim.api.nvim_create_autocmd('TextYankPost', {callback = function() vim.highlight.on_yank() end, group = vim.api.nvim_create_augroup('YankHighlight', {clear = true }), pattern = '*',})
+require('telescope').setup()
 pcall(require('telescope').load_extension, 'fzf') pcall(require('telescope').load_extension, 'aerial')
+
 vim.keymap.set('n', '<leader>?', require('telescope.builtin').oldfiles, { desc = '[?] Find recently opened files' })
 vim.keymap.set('n', '<leader><space>', require('telescope.builtin').buffers, { desc = '[ ] Find existing buffers' })
 vim.keymap.set('n', '<leader>/', function() require('telescope.builtin').current_buffer_fuzzy_find(require('telescope.themes').get_dropdown 
@@ -54,9 +57,10 @@ vim.keymap.set('n', "<leader>k", vim.cmd.UndotreeToggle)
 vim.keymap.set('n', '<leader>nr', ':set number relativenumber<cr>', {})
 vim.keymap.set('n', '<leader>na', ':set number norelativenumber<cr>', {})
 vim.api.nvim_set_keymap("n", "<Leader>cc", ":lua require('neogen').generate()<CR>", { noremap = true, silent = true })
+
 vim.defer_fn(function()
   require('nvim-treesitter.configs').setup {
-    ensure_installed = { 'latex', 'c', 'cpp', 'lua', 'python', 'rust', 'vimdoc', 'bash' }, ignore_install = {'javascript', 'vim'},
+    ensure_installed = { 'latex', 'c', 'cpp', 'python', 'rust', 'bash' }, ignore_install = {'javascript', 'vim'},
     auto_install = false, sync_install = false, modules = {}, highlight = { enable = true }, indent = { enable = true },
     incremental_selection = { enable = true,
       keymaps = {init_selection = '<c-space>', node_incremental = '<c-space>', scope_incremental = '<c-s>', node_decremental = '<M-space>',},},
@@ -68,16 +72,16 @@ vim.defer_fn(function()
         goto_previous_start = {['[m'] = '@function.outer',['[['] = '@class.outer',},},},} end, 0)
 local on_attach = function(_, bufnr)
   local nmap = function(keys, func, desc) vim.keymap.set('n', keys, func, { buffer = bufnr, desc = desc }) end
-  nmap('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
   nmap('K', vim.lsp.buf.hover, 'Hover Documentation')
   nmap('<C-k>', vim.lsp.buf.signature_help, 'Signature Documentation')
   nmap('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
   nmap('gd', require('telescope.builtin').lsp_definitions, '[G]oto [D]efinition')
   nmap('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
   nmap('gI', require('telescope.builtin').lsp_implementations, '[G]oto [I]mplementation')
-  nmap('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')end
+  nmap('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
+end
 require('mason').setup() require('mason-lspconfig').setup()
-local servers = { texlab = {}, pyright = {}, ruff_lsp = {}, html = {}, rust_analyzer = {}, lua_ls = {Lua = {workspace = { checkThirdParty = false }, telemetry = { enable = false },},},}
+local servers = {texlab = {}, pyright = {}, ruff_lsp = {}, html = {}, rust_analyzer = {},}
 local capabilities = vim.lsp.protocol.make_client_capabilities() capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
 local mason_lspconfig = require 'mason-lspconfig'
 mason_lspconfig.setup {ensure_installed = vim.tbl_keys(servers),}
