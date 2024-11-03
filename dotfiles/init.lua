743ef2eb30e4883f3ecb0a3479b0b9a53a072686
yobibyte <y0b1byte@proton.me>
2024-11-03 22:04:15 +0000

    layout

--- dotfiles/init.lua
+++ dotfiles/init.lua
@@ -69,7 +69,9 @@ vim.defer_fn(function()
         keymaps = {['aa'] = '@parameter.outer', ['ia'] = '@parameter.inner', ['af'] = '@function.outer',
                    ['if'] = '@function.inner',  ['ac'] = '@class.outer',     ['ic'] = '@class.inner',},},
       move = { enable = true, set_jumps = true, goto_next_start = {    [']m'] = '@function.outer',[']]'] = '@class.outer',},
-        goto_previous_start = {['[m'] = '@function.outer',['[['] = '@class.outer',},},},} end, 0)
+        goto_previous_start = {['[m'] = '@function.outer',['[['] = '@class.outer',},},},} end, 0
+)
+
 local on_attach = function(_, bufnr)
   local nmap = function(keys, func, desc) vim.keymap.set('n', keys, func, { buffer = bufnr, desc = desc }) end
   nmap('K', vim.lsp.buf.hover, 'Hover Documentation')
@@ -80,6 +82,7 @@ local on_attach = function(_, bufnr)
   nmap('gI', require('telescope.builtin').lsp_implementations, '[G]oto [I]mplementation')
   nmap('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
 end
+
 require('mason').setup() require('mason-lspconfig').setup()
 local servers = {texlab = {}, pyright = {}, ruff_lsp = {}, html = {}, rust_analyzer = {},}
 local capabilities = vim.lsp.protocol.make_client_capabilities() capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
@@ -87,6 +90,7 @@ local mason_lspconfig = require 'mason-lspconfig'
 mason_lspconfig.setup {ensure_installed = vim.tbl_keys(servers),}
 mason_lspconfig.setup_handlers {function(server_name) require('lspconfig')[server_name].setup {
   capabilities = capabilities, on_attach = on_attach, settings = servers[server_name], filetypes = (servers[server_name] or {}).filetypes, } end,}
+
 local cmp = require 'cmp' local luasnip = require 'luasnip'
 luasnip.config.setup {}
 cmp.setup { snippet = {expand = function(args) luasnip.lsp_expand(args.body) end,},
@@ -95,5 +99,6 @@ cmp.setup { snippet = {expand = function(args) luasnip.lsp_expand(args.body) end
     ['<C-n>'] = cmp.mapping.select_next_item(), ['<C-p>'] = cmp.mapping.select_prev_item(),
     ['<CR>'] = cmp.mapping.confirm {behavior = cmp.ConfirmBehavior.Replace,select = true,},},
   sources = {{ name = 'nvim_lsp' },{ name = 'luasnip' },},}
+
 vim.api.nvim_create_autocmd({ "BufEnter", "CursorHold", "CursorHoldI", "FocusGained" }, {command = "if mode() != 'c' | checktime | endif", pattern = { "*" },})
 vim.cmd 'colorscheme retrobox'
