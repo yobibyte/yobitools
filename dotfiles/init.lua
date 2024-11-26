115d6549d0c180dce57b3266327047c077a24ad7
yobibyte <y0b1byte@proton.me>
2024-11-26 22:35:59 +0000

    Update ruff_lsp->ruff

--- dotfiles/init.lua
+++ dotfiles/init.lua
@@ -84,7 +84,7 @@ local on_attach = function(_, bufnr)
 end
 
 require('mason').setup() require('mason-lspconfig').setup()
-local servers = {texlab = {}, pyright = {}, ruff_lsp = {}, html = {}, rust_analyzer = {},}
+local servers = {texlab = {}, pyright = {}, ruff = {}, html = {}, rust_analyzer = {},}
 local capabilities = vim.lsp.protocol.make_client_capabilities() capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
 local mason_lspconfig = require 'mason-lspconfig'
 mason_lspconfig.setup {ensure_installed = vim.tbl_keys(servers),}
