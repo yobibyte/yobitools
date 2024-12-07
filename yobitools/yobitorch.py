c5c71b1c44f0caf18d370cfecbdafca65ab1844d
yobibyte <y0b1byte@proton.me>
2024-12-07 00:27:23 +0000

    remove old code

--- yobitools/yobitorch.py
+++
@@ -1,7 +0,0 @@
-"""Useful utils for working with pytorch"""
-
-def get_model_size(model):
-    """get number of parameters in a pytorch module"""
-    return sum(p.numel() for p in model.parameters() if p.requires_grad)
- 
-
