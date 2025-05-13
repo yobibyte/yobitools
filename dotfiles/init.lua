a6a56892dcb9a625074a2cc748f7d081c7ecb065
yobibyte <y0b1byte@proton.me>
2025-05-13 21:23:53 +0100

    Remove hotkey

--- dotfiles/init.lua
+++ dotfiles/init.lua
@@ -20,7 +20,7 @@ require('lazy').setup({
   {'yobibyte/nvim-treesitter', 
     dependencies = {'yobibyte/nvim-treesitter-textobjects',}, build = ':TSUpdate', main='nvim-treesitter.configs', 
     opts = {ensure_installed = { 'c', 'cpp', 'python', 'rust', 'bash', 'zig' }, ignore_install = {'javascript', 'vim'}, auto_install = false, sync_install = false, modules = {}, highlight = { enable = false }, indent = { enable = true },
-      incremental_selection = { enable = true, keymaps = {init_selection = '<c-space>', node_incremental = '<c-space>', scope_incremental = '<c-s>', node_decremental = '<M-space>',},},
+      incremental_selection = { enable = true, keymaps = {init_selection = '<c-space>', node_incremental = '<c-space>', node_decremental = '<M-space>',},},
       textobjects = { select = { enable = true, lookahead = true, keymaps = {['aa'] = '@parameter.outer', ['ia'] = '@parameter.inner', ['af'] = '@function.outer', ['if'] = '@function.inner',  ['ac'] = '@class.outer', ['ic'] = '@class.inner',},},
 		     move = { enable = true, set_jumps = true, goto_next_start = {    [']m'] = '@function.outer',[']]'] = '@class.outer',}, goto_previous_start = {['[m'] = '@function.outer',['[['] = '@class.outer',},},},},},
   {"yobibyte/neogen", dependencies = "yobibyte/nvim-treesitter", config = true, languages = { python = { template = { annotation_convention = "google_docstrings" } } },}}, {})
