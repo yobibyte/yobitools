c18b84471977127d8f2c5a3381856a42ce81a274
yobibyte <y0b1byte@proton.me>
2025-05-13 12:33:03 +0100

    default

--- dotfiles/init.lua
+++ dotfiles/init.lua
@@ -7,7 +7,6 @@ vim.o.undofile = true
 vim.o.ignorecase = true
 vim.o.smartcase = true
 vim.o.timeoutlen = 300
-vim.o.termguicolors = true
 vim.o.foldlevel = 99
 vim.o.foldlevelstart = 4
 vim.o.foldnestmax = 4
