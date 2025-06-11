aab85bd420ebcbbfc0720723011adde45484fd02
yobibyte <y0b1byte@proton.me>
2025-06-11 01:06:32 +0100

    Compress

--- dotfiles/init.lua
+++ dotfiles/init.lua
@@ -6,61 +6,34 @@ vim.o.undofile = true
 vim.o.ignorecase = true
 vim.o.smartcase = true
 vim.o.timeoutlen = 300
-vim.g.netrw_banner = 0
-vim.opt.path:append("**")
-vim.opt.wildignore:append {"*.venv/*", "*/.git/*", "*/target/*",}
 vim.cmd("syntax off")
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({ "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable", lazypath, })
-end
+if not vim.loop.fs_stat(lazypath) then vim.fn.system({ "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable", lazypath, }) end
 vim.opt.rtp:prepend(lazypath)
 require("lazy").setup({
   "yobibyte/vim-fugitive", "yobibyte/vim-sleuth", "yobibyte/Comment.nvim",
-  { "yobibyte/nvim-treesitter",
-    dependencies = { "yobibyte/nvim-treesitter-textobjects" },
-    build = ":TSUpdate", main = "nvim-treesitter.configs",
-    opts = {
-      ensure_installed = { "c", "cpp", "python", "rust", "bash", "zig" },
-      auto_install = true, sync_install = false, indent = { enable = true },
-      incremental_selection = { enable = true,
-        keymaps = {init_selection = "<c-space>", node_incremental = "<c-space>", node_decremental = "<M-space>",},
-      },
-    },
-  },
+  { "yobibyte/nvim-treesitter", build = ":TSUpdate", main = "nvim-treesitter.configs",
+    opts = { ensure_installed = { "c", "cpp", "python", "rust", "bash", "zig" }, auto_install = true, sync_install = false, indent = { enable = true },
+             incremental_selection = { enable = true, keymaps = {init_selection = "<c-space>", node_incremental = "<c-space>", node_decremental = "<M-space>",}, }, }, },
   {'yobibyte/telescope.nvim', branch = '0.1.x', dependencies = { 'yobibyte/plenary.nvim', {'yobibyte/telescope-fzf-native.nvim', build = 'make', cond = function() return vim.fn.executable 'make' == 1 end,},},},
-  { "yobibyte/neogen", dependencies = "yobibyte/nvim-treesitter", config = true, languages = { python = { template = { annotation_convention = "google_docstrings" } }, }, },
+  {"yobibyte/neogen", dependencies = "yobibyte/nvim-treesitter", config = true, languages = { python = { template = { annotation_convention = "google_docstrings" } }, }, },
 }, {})
-vim.api.nvim_create_autocmd("TextYankPost", {
-  callback = function() vim.highlight.on_yank() end, group = vim.api.nvim_create_augroup("YankHighlight", { clear = true }), pattern = "*",
-})
+vim.api.nvim_create_autocmd("TextYankPost", { callback = function() vim.highlight.on_yank() end, group = vim.api.nvim_create_augroup("YankHighlight", { clear = true }), pattern = "*", })
 vim.cmd("colorscheme retrobox")
 vim.api.nvim_set_hl(0, "Normal", { fg = "#ffaf00" })
-vim.api.nvim_create_autocmd("FileType", {
-  pattern = "*",
-  callback = function()
-    vim.cmd("TSEnable highlight")
-    vim.cmd("TSDisable highlight")
-  end,
-})
+vim.api.nvim_create_autocmd("FileType", { pattern = "*", callback = function() vim.cmd("TSEnable highlight") vim.cmd("TSDisable highlight") end, })
 vim.keymap.set( "n", "<leader>cc", ":lua require('neogen').generate()<CR>", { noremap = true, silent = true })
 vim.keymap.set("i", "jj", "<Esc>")
 vim.keymap.set("n", "<leader>e", ":Explore<cr>")
-vim.keymap.set("n", "<leader>gg", ":find ")
 vim.keymap.set("n", "<leader>gp", function() vim.cmd( "edit " .. vim.fn.system("python3 -c 'import site; print(site.getsitepackages()[0])'") :gsub("%s+$", "") .. "/.") end )
-vim.keymap.set("n", "<leader>gr", 
-  function()
-    local registry = os.getenv("CARGO_HOME") or (os.getenv("HOME") .. "/.cargo") .. "/registry/src"
-    vim.cmd("edit " .. registry .. "/" .. vim.fn.systemlist("ls -1 " .. registry)[1])
-  end
-)
+vim.keymap.set("n", "<leader>gr", function() local registry = os.getenv("CARGO_HOME") or (os.getenv("HOME") .. "/.cargo") .. "/registry/src" vim.cmd("edit " .. registry .. "/" .. vim.fn.systemlist("ls -1 " .. registry)[1]) end)
 vim.keymap.set("n", "<leader>n", ":set number!<cr>")
 vim.keymap.set('n', '<leader><space>', require('telescope.builtin').buffers)
 vim.keymap.set('n', '<leader>sf',      require('telescope.builtin').find_files)
 vim.keymap.set('n', '<leader>sg',      require('telescope.builtin').live_grep)
 vim.keymap.set('n', '<leader>sr',      require('telescope.builtin').resume, {})
-vim.keymap.set('n', '<leader>so',       require('telescope.builtin').oldfiles)
-vim.keymap.set('n', '<leader>sb',       require('telescope.builtin').current_buffer_fuzzy_find, {})
+vim.keymap.set('n', '<leader>so',      require('telescope.builtin').oldfiles)
+vim.keymap.set('n', '<leader>sb',      require('telescope.builtin').current_buffer_fuzzy_find, {})
 vim.keymap.set('n', '<leader>g', ':Git<CR>', {})
 vim.keymap.set('n', '<leader>jg', ':vertical Git<CR>', {})
 vim.keymap.set('n', '<leader>df', function() require('telescope.builtin').find_files({cwd = vim.fn.expand('%:p:h'), no_ignore=true}) end, { noremap = true, silent = true })
@@ -70,12 +43,8 @@ vim.keymap.set("n", "<C-j>", ":move .+1<CR>", {})
 vim.keymap.set("n", "<C-k>", ":move .-2<CR>", {})
 vim.keymap.set('v', '<C-j>', ":move '>+1<CR>gv", { noremap = true, silent = true })
 vim.keymap.set('v', '<C-k>', ":move '<-2<CR>gv", { noremap = true, silent = true })
-
--- linting
 vim.keymap.set("n", "<leader>bb", ":!black %<cr>")
-vim.keymap.set("n", "<leader>br", function()
-  local output = vim.fn.system({ "ruff", "check", vim.fn.expand("%") })
-  vim.cmd("vnew")
-  vim.api.nvim_buf_set_lines(0, 0, -1, false, vim.split(output, "\n"))
+vim.keymap.set("n", "<leader>br", function() vim.cmd("vnew")
+  vim.api.nvim_buf_set_lines(0, 0, -1, false, vim.split(vim.fn.system({ "ruff", "check", vim.fn.expand("#") }), "\n"))
   vim.bo.buftype = "nofile" vim.bo.bufhidden = "wipe" vim.bo.swapfile = false
 end, {})
