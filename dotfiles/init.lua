3faa360f6c93075bb65e9092d5f4349339b0abf8
yobibyte <y0b1byte@proton.me>
2025-06-11 17:54:09 +0100

    Simplify neogen setup.

--- dotfiles/init.lua
+++ dotfiles/init.lua
@@ -11,11 +11,9 @@ local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then vim.fn.system({ "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable", lazypath, }) end
 vim.opt.rtp:prepend(lazypath)
 require("lazy").setup({
-  "yobibyte/vim-fugitive", 
-  {"yobibyte/nvim-treesitter", build = ":TSUpdate", main = "nvim-treesitter.configs", opts = { ensure_installed = { "c", "cpp", "python", "rust", "bash", "zig" }, auto_install = true, sync_install = false, indent = { enable = true },
-             incremental_selection = { enable = true, keymaps = {init_selection = "<c-space>", node_incremental = "<c-space>", node_decremental = "<M-space>",}, }, }, },
+  "yobibyte/vim-fugitive", {"yobibyte/neogen", config = true,},
+  {"yobibyte/nvim-treesitter", build = ":TSUpdate", main = "nvim-treesitter.configs", opts = { ensure_installed = { "c", "cpp", "python", "rust", "bash", "zig" }, auto_install = true, sync_install = false, indent = { enable = true }, incremental_selection = { enable = true, keymaps = {init_selection = "<c-space>", node_incremental = "<c-space>", node_decremental = "<M-space>",},},},},
   {'yobibyte/telescope.nvim', branch = '0.1.x', dependencies = { 'yobibyte/plenary.nvim', {'yobibyte/telescope-fzf-native.nvim', build = 'make', cond = function() return vim.fn.executable 'make' == 1 end,},},},
-  {"yobibyte/neogen", dependencies = "yobibyte/nvim-treesitter", config = true, languages = { python = { template = { annotation_convention = "google_docstrings" } }, }, },
 }, {})
 vim.api.nvim_create_autocmd("TextYankPost", { callback = function() vim.highlight.on_yank() end, group = vim.api.nvim_create_augroup("YankHighlight", { clear = true }), pattern = "*", })
 vim.cmd("colorscheme retrobox") vim.api.nvim_set_hl(0, "Normal", { fg = "#ffaf00" }) vim.api.nvim_create_autocmd("FileType", { pattern = "*", callback = function() vim.cmd("TSEnable highlight") vim.cmd("TSDisable highlight") end, })
@@ -92,4 +90,3 @@ vim.api.nvim_create_autocmd("BufReadPost", { callback = function()
     end
   end,
 })
-
