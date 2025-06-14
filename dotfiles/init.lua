d6f7e62d501d659fcf6f4b59e9d196fce80875a3
yobibyte <y0b1byte@proton.me>
2025-06-14 22:18:08 +0100

    simplify

--- dotfiles/init.lua
+++ dotfiles/init.lua
@@ -29,10 +29,7 @@ vim.api.nvim_create_autocmd("BufReadPost", { callback = function()
             space_count = space_count + 1
             local len = #indent
             if not min_indent or len < min_indent then min_indent = len end
-          end
-        end
-      end
-    end
+    end end end end
     if tab_count > space_count then
       vim.opt_local.expandtab = false
     else
@@ -41,9 +38,7 @@ vim.api.nvim_create_autocmd("BufReadPost", { callback = function()
       vim.opt_local.shiftwidth = min_indent
       vim.opt_local.tabstop = min_indent
       vim.opt_local.softtabstop = min_indent
-    end
-  end,
-})
+end end, })
 
 local function scratch_to_quickfix()
   local prev_bufnr = vim.fn.bufnr('#')
@@ -54,24 +49,21 @@ local function scratch_to_quickfix()
     if line ~= "" then
       local filename, lnum, text = line:match("^([^:]+):(%d+):(.*)$")
       if filename and lnum then
-        --for grep filename:line:text
+        -- for grep filename:line:text
         table.insert(items, { filename = vim.fn.fnamemodify(filename, ":p"), lnum = tonumber(lnum), col = 1, text = text, })
       else
         local lnum, text = line:match("^(%d+):(.*)$")
         if lnum and text then
+          -- for current buffer grep
           table.insert(items, { filename = orig_name, lnum = tonumber(lnum), col = 1, text = text, })
         else
           -- for find results, only fnames
           table.insert(items, { filename = vim.fn.fnamemodify(line, ":p"), lnum = 1, col = 1, text = "", })
-        end
-      end
-    end
-  end
+  end end end end
 
   vim.api.nvim_buf_delete(bufnr, { force = true })
   vim.fn.setqflist(items, "r")
-  vim.cmd("copen")
-  vim.cmd("cc")
+  vim.cmd("copen | cc")
 end
 
 local function extcmd(cmd, use_list, quickfix) 
@@ -102,8 +94,7 @@ end, { nargs = "+", })
 vim.api.nvim_create_user_command("GrepTextSearch", function(opts)
   local path = opts.bang and vim.fn.expand("%:p:h") or vim.fn.getcwd()
   local excludes = "--exclude-dir='*target*' --exclude-dir=.git --exclude-dir='*.egg-info' --exclude-dir='__pycache__'"
-  if vim.bo.filetype == "netrw" then path = vim.b.netrw_curdir else excludes = excludes .. " --exclude-dir=.venv"
-  end
+  if vim.bo.filetype == "netrw" then path = vim.b.netrw_curdir else excludes = excludes .. " --exclude-dir=.venv" end
   extcmd("grep -IEnr "  .. excludes .. " '" .. opts.args .. "' " .. path, true, true)
 end, { nargs = "+"})
 
@@ -125,21 +116,18 @@ vim.keymap.set("v", "<C-k>", ":move '<-2<CR>gv")
 vim.keymap.set("n", "<leader>e", ":Explore<cr>")
 vim.keymap.set("n", "<leader>w", ":set number!<cr>")
 vim.keymap.set("n", "<leader>so",":browse oldfiles<cr>")
-vim.keymap.set("n", "<leader>x", scratch_to_quickfix)
-vim.keymap.set("n", "<leader>o", function() vim.cmd.edit(vim.fn.fnameescape(vim.fn.trim(vim.fn.getreg("+")))) end)
-vim.keymap.set("n", "<leader>gl",function() extcmd({"git", "log"}) end)
-vim.keymap.set("n", "<leader>gd",function() extcmd({"git", "diff"}) end)
-vim.keymap.set("n", "<leader>gb",function() extcmd({"git", "blame", vim.fn.expand("%")}) end)
-vim.keymap.set("n", "<leader>gs",function() extcmd({"git", "show", vim.fn.expand("<cword>")}) end)
-vim.keymap.set("n", "<leader>gp",function() vim.cmd("edit " .. vim.fn.system("python3 -c 'import site; print(site.getsitepackages()[0])'") :gsub("%s+$", "") .. "/.") end)
-vim.keymap.set("n", "<leader>gr",function() local registry = os.getenv("CARGO_HOME") or (os.getenv("HOME") .. "/.cargo") .. "/registry/src" vim.cmd( "edit " .. registry .. "/" .. vim.fn.systemlist("ls -1 " .. registry)[1]) end)
+vim.keymap.set("n", "<leader>x",  scratch_to_quickfix)
+vim.keymap.set("n", "<leader>o",  function() vim.cmd.edit(vim.fn.fnameescape(vim.fn.trim(vim.fn.getreg("+")))) end)
+vim.keymap.set("n", "<leader>gl", function() extcmd({"git", "log"}) end)
+vim.keymap.set("n", "<leader>gd", function() extcmd({"git", "diff"}) end)
+vim.keymap.set("n", "<leader>gb", function() extcmd({"git", "blame", vim.fn.expand("%")}) end)
+vim.keymap.set("n", "<leader>gs", function() extcmd({"git", "show", vim.fn.expand("<cword>")}) end)
+vim.keymap.set("n", "<leader>gp", function() vim.cmd("edit " .. vim.fn.system("python3 -c 'import site; print(site.getsitepackages()[0])'") :gsub("%s+$", "") .. "/.") end)
+vim.keymap.set("n", "<leader>gr", function() local registry = os.getenv("CARGO_HOME") or (os.getenv("HOME") .. "/.cargo") .. "/registry/src" vim.cmd( "edit " .. registry .. "/" .. vim.fn.systemlist("ls -1 " .. registry)[1]) end)
 vim.keymap.set("n", "<leader>sf", function() vim.ui.input({ prompt = "> " }, function(name) if name then vim.cmd("FileSearch " .. name) end end) end)
 vim.keymap.set("n", "<leader>sg", function() vim.ui.input({ prompt = "> " }, function(name) if name then vim.cmd("GrepTextSearch " .. name) end end) end)
 vim.keymap.set("n", "<leader>bb",":!black %<cr>")
 vim.keymap.set("n", "<leader>br",function() extcmd({ "ruff", "check", vim.fn.expand("#") }) end)
 vim.keymap.set("n", "<leader>ss", function()
-  vim.ui.input({ prompt = "> " }, function(pattern)
-    if not pattern or pattern == "" then return end
-    extcmd("grep -in '" .. pattern .. "' " .. vim.fn.shellescape(vim.api.nvim_buf_get_name(0)), true, false)
-  end)
+  vim.ui.input({ prompt = "> " }, function(pattern) if not pattern or pattern == "" then return end extcmd("grep -in '" .. pattern .. "' " .. vim.fn.shellescape(vim.api.nvim_buf_get_name(0)), true, false) end)
 end)
