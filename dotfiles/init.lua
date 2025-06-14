c8d17e9af9128cca6cb0a3b7856cfed62b823382
yobibyte <y0b1byte@proton.me>
2025-06-14 18:08:21 +0100

    Simplify telescope funcs

--- dotfiles/init.lua
+++ dotfiles/init.lua
@@ -8,6 +8,9 @@ vim.o.smartcase = true
 vim.o.autoread = true
 vim.o.timeoutlen = 300
 vim.o.wildignorecase = true
+vim.g.netrw_banner = 0
+vim.opt.path:append("**")
+vim.opt.wildignore:append {"*.venv/*", "*/.git/*", "*/target/*",}
 vim.cmd("syntax off") vim.cmd("colorscheme retrobox") vim.api.nvim_set_hl(0, "Normal", { fg = "#ffaf00" })
 local pluginpath = vim.fn.stdpath("data") .. "/site/pack/plugins/start/"
 if not vim.loop.fs_stat(pluginpath) then
@@ -18,6 +21,8 @@ end
 local function scratch() vim.bo.buftype = "nofile" vim.bo.bufhidden = "wipe" vim.bo.swapfile = false end
 vim.keymap.set("i", "jj", "<Esc>")
 vim.keymap.set("n", ";;", ":w<cr>")
+vim.keymap.set("n", "<leader>q", ":q!<cr>")
+vim.keymap.set("n", "<leader>d", ":bd<cr>")
 vim.keymap.set("n", "<leader><space>", ":b ")
 vim.keymap.set("n", "<C-n>", ":bn<cr>", {})
 vim.keymap.set("n", "<C-p>", ":bp<cr>", {})
@@ -27,17 +32,26 @@ vim.keymap.set("v", "<C-j>", ":move '>+1<CR>gv", { noremap = true, silent = true
 vim.keymap.set("v", "<C-k>", ":move '<-2<CR>gv", { noremap = true, silent = true })
 vim.keymap.set("n", "<leader>e", ":Explore<cr>")
 vim.keymap.set("n", "<leader>n", ":set number!<cr>")
+vim.keymap.set("n", "<leader>so",":browse oldfiles<cr>")
+vim.keymap.set("n", "<leader>o",       function() local file = vim.fn.getreg('+') vim.cmd.edit(vim.fn.fnameescape(vim.fn.trim(file))) end, {})
 vim.keymap.set("n", "<leader>gl",      function() vim.cmd("vnew") vim.api.nvim_buf_set_lines( 0, 0, -1, false, vim.split(vim.fn.system({"git", "log"}), "\n")) scratch() end, {})
 vim.keymap.set("n", "<leader>gd",      function() vim.cmd("vnew") vim.api.nvim_buf_set_lines( 0, 0, -1, false, vim.split(vim.fn.system({"git", "diff"}), "\n")) scratch() end, {})
 vim.keymap.set("n", "<leader>gb",      function() local fpath = vim.fn.expand("%") vim.cmd("vnew") vim.api.nvim_buf_set_lines( 0, 0, -1, false, vim.split(vim.fn.system({"git", "blame", fpath}), "\n")) scratch() end, {})
 vim.keymap.set("n", "<leader>gs",      function() local hash = vim.fn.expand("<cword>") vim.cmd("vnew") vim.api.nvim_buf_set_lines( 0, 0, -1, false, vim.split(vim.fn.system({"git", "show", hash}), "\n")) scratch() end, {})
-vim.keymap.set("n", "<leader>sg",      require("telescope.builtin").live_grep)
-vim.keymap.set("n", "<leader>sr",      require("telescope.builtin").resume, {})
-vim.keymap.set("n", "<leader>sf",      function() require("telescope.builtin").find_files { previewer = false, layout_strategy = "center", layout_config = { height = 0.4, }, } end)
-vim.keymap.set("n", "<leader>so",      function() require("telescope.builtin").oldfiles { previewer = false, layout_strategy = "center", layout_config = { height = 0.4, }, } end)
+vim.keymap.set("n", "<leader>sf", function() 
+  local config = { previewer = false, layout_strategy = "center", layout_config = { height = 0.4, }, } 
+  if vim.bo.filetype == "netrw" then config.cwd = vim.fn.expand("%:p:h") config.no_ignore = true end
+  require("telescope.builtin").find_files(config)
+end)
+vim.keymap.set("n", "<leader>sg", function() 
+  local config = {}
+  if vim.bo.filetype == "netrw" then
+    config.search_dirs = {vim.b.netrw_curdir} 
+    config.additional_args = function() return { "--hidden", "--no-ignore" } end
+  end
+  require("telescope.builtin").live_grep(config)
+end)
 vim.keymap.set("n", "<leader>sb",      function() require("telescope.builtin").live_grep( { search_dirs = { vim.fn.expand("%:p")  }}) end )
-vim.keymap.set("n", "<leader>df",      function() require("telescope.builtin").find_files({ cwd = vim.fn.expand("%:p:h"), no_ignore = true, }) end, { noremap = true, silent = true })
-vim.keymap.set("n", "<leader>ds",      function() require("telescope.builtin").live_grep({ cwd = vim.fn.expand("%:p:h"), additional_args = function() return { "--hidden", "--no-ignore" } end, }) end, { noremap = true, silent = true })
 vim.keymap.set("n", "<leader>gp",      function() vim.cmd( "edit " .. vim.fn .system("python3 -c 'import site; print(site.getsitepackages()[0])'") :gsub("%s+$", "") .. "/.") end)
 vim.keymap.set("n", "<leader>gr",      function() local registry = os.getenv("CARGO_HOME") or (os.getenv("HOME") .. "/.cargo") .. "/registry/src" vim.cmd( "edit " .. registry .. "/" .. vim.fn.systemlist("ls -1 " .. registry)[1]) end)
 vim.keymap.set("n", "<leader>bb",      ":!black %<cr>")
