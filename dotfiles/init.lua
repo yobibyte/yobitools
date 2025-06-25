86a766ee65d9d0edbaba954861dd2f3dbed36e64
yobibyte <y0b1byte@proton.me>
2025-06-25 17:50:29 +0100

    simplify

--- dotfiles/init.lua
+++ dotfiles/init.lua
@@ -10,7 +10,7 @@ local function cqf(cls) local items, bufnr = {}, vim.api.nvim_get_current_buf() 
   if f and ln then table.insert(items, { filename = vim.fn.fnamemodify(f, ":p"), lnum = ln, text = text, }) else local ln, text = line:match("^(%d+):(.*)$") table.insert(items, { filename = vim.fn.bufname(vim.fn.bufnr("#")), lnum = ln, text = text, }) end end end 
   vim.api.nvim_buf_delete(bufnr, { force = true }) vim.fn.setqflist(items, "r") vim.cmd("copen | cc") if cls then vim.cmd("cclose") end end
 local function extc(cmd, qf, cls) o = vim.fn.systemlist(cmd) if o and #o > 0 then vim.cmd("vnew") vim.api.nvim_buf_set_lines( 0, 0, -1, false, o) vim.bo.buftype = "nofile" vim.bo.bufhidden = "wipe" vim.bo.swapfile = false if qf then cqf(cls) end end end
-vim.keymap.set("n", "<C-n>", ":cn<cr>")     vim.keymap.set("n", "<C-p>", ":cp<cr>")
+vim.keymap.set("n", "<C-n>", ":cn<cr>") vim.keymap.set("n", "<C-p>", ":cp<cr>") vim.keymap.set("n", "<leader>d", ":bd<cr>")
 vim.keymap.set("x", "<leader>p", "\"_dP")
 vim.keymap.set("n", "<leader><space>", ":ls<cr>:b ")
 vim.keymap.set("n", "<leader>e", ":Explore<cr>")
@@ -21,6 +21,5 @@ vim.keymap.set("n", "<leader>gd", function() if vim.bo.filetype == "rust" then v
 vim.keymap.set("n", "<leader>ss", function() vim.ui.input({ prompt = "> " }, function(p) if p then extc("grep -in '" .. p .. "' " .. vim.fn.shellescape(vim.api.nvim_buf_get_name(0))) end end) end)
 vim.keymap.set("n", "<leader>sg", function() vim.ui.input({ prompt = "> " }, function(p) if p then local path, ex = pre_search(true) extc(string.format("grep -IEnr %s '%s' %s", ex, p, path), true) end end) end)
 vim.keymap.set("n", "<leader>sf", function() vim.ui.input({ prompt = "> " }, function(p) if p then local path, ex = pre_search(false) extc(string.format("find %s %s -path '*%s*' -print | awk '{ print $0 \":1: \" }'", vim.fn.shellescape(path), ex, p), true, true) end end) end)
-vim.keymap.set("n", "<leader>l", function() local ft = vim.bo.filetype if ft == "rust" then extc("cargo check && cargo clippy") elseif ft == "python" then extc("ruff check --output-format=concise --quiet " .. vim.fn.expand("%"), true) vim.cmd("edit") end end)
 vim.keymap.set("n", "<leader>c", function() vim.ui.input({ prompt = "> " }, function(c) if c then extc(c) end end) end)
 vim.keymap.set('n', '<leader>y', function() vim.fn.setreg('+', vim.fn.expand('%:p')) end)
