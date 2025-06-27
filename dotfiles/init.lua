a8e6c57bdebb8be0b3e870e5022c943b1a6e0e1c
yobibyte <y0b1byte@proton.me>
2025-06-27 22:41:25 +0100

    compress

--- dotfiles/init.lua
+++ dotfiles/init.lua
@@ -2,7 +2,7 @@ vim.o.undofile    = true
 vim.opt.expandtab = true   
 vim.o.clipboard   = "unnamedplus"
 vim.cmd("syntax off | colorscheme retrobox | highlight Normal guifg=#ffaf00 guibg=#282828")
-vim.api.nvim_create_autocmd("TextYankPost", { callback = function() vim.highlight.on_yank() end, group = vim.api.nvim_create_augroup("YankHighlight", { clear = true }), pattern = "*" })
+vim.api.nvim_create_autocmd("TextYankPost", { callback = function() vim.highlight.on_yank() end})
 vim.api.nvim_create_autocmd({"BufEnter", "FileType"}, { callback = function() 
     local i = 4 for _, l in ipairs(vim.api.nvim_buf_get_lines(0, 0, 50, false)) do local c = l:match("^(%s+)%S") if c then i = math.min(i, #c) end end 
     vim.opt_local.shiftwidth, vim.opt_local.tabstop, vim.opt_local.softtabstop = i, i, i 
@@ -26,7 +26,8 @@ vim.keymap.set('n', '<space>y', function() vim.fn.setreg('+', vim.fn.expand('%:p
 vim.keymap.set("n", "<space>c", function() vim.ui.input({ prompt = "> " }, function(c) if c then ext(c) end end) end)
 vim.keymap.set("n", "<space>b", function() vim.ui.input({ prompt = "> " }, function(p) if p then ext("grep -in '" .. p .. "' " .. vim.fn.shellescape(vim.fn.bufname("%"))) end end) end)
 vim.keymap.set("n", "<space>g", function() vim.ui.input({ prompt = "> " }, function(p) if p then 
-  if ext(string.format("grep -IEnr %s '%s' %s", vim.b._g_excs, p, vim.b._search_path), true) then vim.cmd("cgetbuffer | bd | copen | cc") end end end) end)
+  if ext(string.format("grep -IEnr %s '%s' %s", vim.b._g_excs, p, vim.b._search_path), true) then
+    vim.cmd("cgetbuffer | bd | copen | cc") end end end) end)
 vim.keymap.set("n", "<space>f", function() vim.ui.input({ prompt = "> " }, function(p) if p then 
   ext(string.format("find %s %s -path '*%s*' -print ", vim.fn.shellescape(vim.b._search_path), vim.b._f_excs, p), true)
   local l = vim.api.nvim_buf_get_lines(0, 0, -1, false) if #l == 1 then vim.cmd("edit " .. vim.fn.fnameescape(l[1])) end end end) end)
