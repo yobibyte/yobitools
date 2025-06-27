75d8e80853935ca519af99dcddb19add4e13e973
yobibyte <y0b1byte@proton.me>
2025-06-27 09:02:16 +0100

    lint

--- dotfiles/init.lua
+++ dotfiles/init.lua
@@ -6,8 +6,8 @@ vim.opt.expandtab    = true
 vim.o.clipboard      = "unnamedplus"
 vim.cmd("syntax off | colorscheme retrobox")
 vim.api.nvim_set_hl(0, "Normal", { fg = "#ffaf00" })
-local default_exclude = { ".git", "*.egg-info", "__pycache__", "wandb", "target", ".venv" }
-local netrw_exclude   = { ".git", "*.egg-info", "__pycache__", "wandb", "target" }
+local def_exc = { ".git", "*.egg-info", "__pycache__", "wandb", "target", ".venv" }
+local net_exc   = { ".git", "*.egg-info", "__pycache__", "wandb", "target" }
 vim.api.nvim_create_autocmd("TextYankPost", {
   callback = function() vim.highlight.on_yank() end,
   group = vim.api.nvim_create_augroup("YankHighlight", { clear = true }),
@@ -37,8 +37,6 @@ local function ext(c, novs)
   end 
   return false
 end
-vim.keymap.set("n", "<C-n>", ":cn<cr>")
-vim.keymap.set("n", "<C-p>", ":cp<cr>") 
 vim.keymap.set("n", "<leader><space>", ":ls<cr>:b ")
 vim.keymap.set('n', '<leader>y', function() vim.fn.setreg('+', vim.fn.expand('%:p')) end)
 vim.keymap.set("n", "<leader>c", function() vim.ui.input({ prompt = "> " }, function(c) if c then ext(c) end end) end)
@@ -46,10 +44,8 @@ vim.keymap.set("n", "<leader>b", function() vim.ui.input({ prompt = "> " },
   function(p) if p then ext("grep -in '" .. p .. "' " .. vim.fn.shellescape(vim.fn.bufname("%"))) end
 end) end)
 vim.keymap.set("n", "<leader>g", function() vim.ui.input({ prompt = "> " }, function(p) if p then 
-    local path, exc, ex = vim.fn.getcwd(), default_exclude, {} 
-    if vim.bo.filetype == "netrw" then 
-      path, exc = vim.b.netrw_curdir, netrw_exclude
-    end 
+    local path, exc, ex = vim.fn.getcwd(), def_exc, {} 
+    if vim.bo.filetype == "netrw" then path, exc = vim.b.netrw_curdir, net_exc end 
     for i=1,#exc do table.insert(ex, string.format("--exclude-dir='%s'", exc[i])) end 
     if ext(string.format("grep -IEnr %s '%s' %s", table.concat(ex, " "), p, path), true) then
       local items = {} 
@@ -63,10 +59,8 @@ vim.keymap.set("n", "<leader>g", function() vim.ui.input({ prompt = "> " }, func
       vim.cmd("copen | cc")  
 end end end) end)
 vim.keymap.set("n", "<leader>f", function() vim.ui.input({ prompt = "> " }, function(p) if p then 
-  local path, exc, ex = vim.fn.getcwd(), default_exclude, {} 
-  if vim.bo.filetype == "netrw" then 
-    path, exc = vim.b.netrw_curdir, netrw_exclude
-  end 
+  local path, exc, ex = vim.fn.getcwd(), def_exc, {} 
+  if vim.bo.filetype == "netrw" then path, exc = vim.b.netrw_curdir, net_exc end 
   for i=1,#exc do table.insert(ex, string.format("-path '*%s*' -prune -o", exc[i])) end 
   ext(string.format("find %s %s -path '*%s*' -print ", vim.fn.shellescape(path), table.concat(ex, " "), p), true)
 end end) end)
@@ -75,5 +69,4 @@ vim.keymap.set("n", "<leader>j", function()
     vim.cmd("edit " .. vim.fn.system("find " .. (os.getenv("CARGO_HOME") or (os.getenv("HOME") .. "/.cargo"))  .. "/registry/src  -maxdepth 1 -mindepth 1 -print -quit"))
   else 
     vim.cmd("edit " .. vim.fn.system("python3 -c 'import site; print(site.getsitepackages()[0])'") :gsub("%s+$", "") .. "/.") 
-  end 
-end)
+end end)
