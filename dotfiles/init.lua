ce0dc264fa8063fd7f931f419813d0aa4ff33052
yobibyte <y0b1byte@proton.me>
2025-06-27 11:07:00 +0100

    Move the search logic to a autocmd

--- dotfiles/init.lua
+++ dotfiles/init.lua
@@ -6,23 +6,33 @@ vim.opt.expandtab    = true
 vim.o.clipboard      = "unnamedplus"
 vim.cmd("syntax off | colorscheme retrobox")
 vim.api.nvim_set_hl(0, "Normal", { fg = "#ffaf00" })
-local def_exc = { ".git", "*.egg-info", "__pycache__", "wandb", "target", ".venv" }
-local net_exc   = { ".git", "*.egg-info", "__pycache__", "wandb", "target" }
 vim.api.nvim_create_autocmd("TextYankPost", {
   callback = function() vim.highlight.on_yank() end,
   group = vim.api.nvim_create_augroup("YankHighlight", { clear = true }),
   pattern = "*"
 })
-vim.api.nvim_create_autocmd("FileType", {
+vim.api.nvim_create_autocmd({"BufEnter", "FileType"}, {
   callback = function() 
     local i = 4 
     for _, l in ipairs(vim.api.nvim_buf_get_lines(0, 0, 50, false)) do 
-      local c = l:match("^(%s+)%S") 
-      if c then i = math.min(i, #c) end
-    end 
+      local c = l:match("^(%s+)%S") if c then i = math.min(i, #c) end end 
     vim.opt_local.shiftwidth=i 
     vim.opt_local.tabstop=i 
     vim.opt_local.softtabstop = i 
+    if vim.bo.filetype == "rust" then 
+      vim.b._reg_dir = vim.fn.system("find $(echo ${CARGO_HOME:-$HOME/.cargo})/registry/src  -maxdepth 1 -mindepth 1 -print -quit")
+    else 
+      vim.b._reg_dir = vim.fn.system("python3 -c 'import site; print(site.getsitepackages()[0])'"):gsub("%s+$", "") end
+    if vim.bo.filetype == "netrw" then
+      vim.b._search_path, excs = vim.b.netrw_curdir, { ".git", "*.egg-info", "__pycache__", "wandb", "target" }
+    else
+      vim.b._search_path, excs = vim.fn.getcwd(), { ".git", "*.egg-info", "__pycache__", "wandb", "target", ".venv" } end
+    local find_excs, grep_excs = {}, {}
+    for i=1,#excs do 
+      table.insert(find_excs, string.format("-path '*%s*' -prune -o", excs[i]))
+      table.insert(grep_excs, string.format("--exclude-dir='%s'", excs[i])) end
+    vim.b._f_excs = table.concat(find_excs, " ")
+    vim.b._g_excs = table.concat(grep_excs, " ")
   end
 })
 local function ext(c, novs) 
@@ -34,31 +44,17 @@ local function ext(c, novs)
     vim.bo.bufhidden = "wipe" 
     vim.bo.swapfile = false 
     return true
-  end 
-  return false
-end
+  else return false end end
 vim.keymap.set("n", "<leader><space>", ":ls<cr>:b ")
+vim.keymap.set("n", "<leader>j", function() vim.cmd("edit " .. vim.b._reg_dir) end)
 vim.keymap.set('n', '<leader>y', function() vim.fn.setreg('+', vim.fn.expand('%:p')) end)
 vim.keymap.set("n", "<leader>c", function() vim.ui.input({ prompt = "> " }, function(c) if c then ext(c) end end) end)
-vim.keymap.set("n", "<leader>b", function() vim.ui.input({ prompt = "> " }, 
-  function(p) if p then ext("grep -in '" .. p .. "' " .. vim.fn.shellescape(vim.fn.bufname("%"))) end
-end) end)
+vim.keymap.set("n", "<leader>b", function() vim.ui.input({ prompt = "> " }, function(p) if p then ext("grep -in '" .. p .. "' " .. vim.fn.shellescape(vim.fn.bufname("%"))) end end) end)
 vim.keymap.set("n", "<leader>g", function() vim.ui.input({ prompt = "> " }, function(p) if p then 
-    local path, exc, ex = vim.fn.getcwd(), def_exc, {} 
-    if vim.bo.filetype == "netrw" then path, exc = vim.b.netrw_curdir, net_exc end 
-    for i=1,#exc do table.insert(ex, string.format("--exclude-dir='%s'", exc[i])) end 
-    if ext(string.format("grep -IEnr %s '%s' %s", table.concat(ex, " "), p, path), true) then
-      vim.cmd("cgetbuffer | bd | copen | cc")  
-end end end) end)
-vim.keymap.set("n", "<leader>f", function() vim.ui.input({ prompt = "> " }, function(p) if p then 
-  local path, exc, ex = vim.fn.getcwd(), def_exc, {} 
-  if vim.bo.filetype == "netrw" then path, exc = vim.b.netrw_curdir, net_exc end 
-  for i=1,#exc do table.insert(ex, string.format("-path '*%s*' -prune -o", exc[i])) end 
-  ext(string.format("find %s %s -path '*%s*' -print ", vim.fn.shellescape(path), table.concat(ex, " "), p), true)
+  if ext(string.format("grep -IEnr %s '%s' %s", vim.b._g_excs, p, vim.b._search_path), true) then vim.cmd("cgetbuffer | bd | copen | cc") end 
 end end) end)
-vim.keymap.set("n", "<leader>j", function() 
-  if vim.bo.filetype == "rust" then 
-    vim.cmd("edit " .. vim.fn.system("find " .. (os.getenv("CARGO_HOME") or (os.getenv("HOME") .. "/.cargo"))  .. "/registry/src  -maxdepth 1 -mindepth 1 -print -quit"))
-  else 
-    vim.cmd("edit " .. vim.fn.system("python3 -c 'import site; print(site.getsitepackages()[0])'") :gsub("%s+$", "") .. "/.") 
-end end)
+vim.keymap.set("n", "<leader>f", function() vim.ui.input({ prompt = "> " }, function(p) if p then 
+  ext(string.format("find %s %s -path '*%s*' -print ", vim.fn.shellescape(vim.b._search_path), vim.b._f_excs, p), true)
+  local l = vim.api.nvim_buf_get_lines(0, 0, -1, false) 
+  if #l == 1 then vim.cmd("edit " .. vim.fn.fnameescape(l[1])) end
+end end) end)
