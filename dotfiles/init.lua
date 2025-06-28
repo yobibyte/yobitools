ab2ed51a49d3357d7a973b7d79ee4cd3199a5e21
yobibyte <y0b1byte@proton.me>
2025-06-28 18:48:28 +0100

    compress

--- dotfiles/init.lua
+++ dotfiles/init.lua
@@ -13,24 +13,21 @@ vim.api.nvim_create_autocmd({"BufEnter", "FileType"}, { callback = function() t(
     if vim.bo.filetype ~= "netrw" then
         excs[#excs+1] = ".venv"
         vim.b._sp = vim.fn.shellescape(vim.fn.getcwd()) end
-    vim.b._f_excs = table.concat(vim.tbl_map(function(e) return "-path '*" .. e .. "*' -prune -o" end, excs), " ")
-    vim.b._g_excs = table.concat(vim.tbl_map(function(e) return "--exclude-dir='" .. e .. "'" end, excs), " ")
+    vim.b._f_ex = table.concat(vim.tbl_map(function(e) return "-path '*" .. e .. "*' -prune -o" end, excs), " ")
+    vim.b._g_ex = table.concat(vim.tbl_map(function(e) return "--exclude-dir='" .. e .. "'" end, excs), " ")
     if vim.treesitter and vim.treesitter.stop then vim.treesitter.stop() end end })
-local function ext(c, novs, autoopen, qf)
+local function ext(c, novs, qf)
     o = vim.fn.systemlist(c) 
     if not (o and #o > 0) then return end
     vim.cmd(novs and "enew" or "vnew") 
     vim.api.nvim_buf_set_lines(0, 0, -1, false, o) 
     vim.bo.buftype, vim.bo.bufhidden, vim.bo.swapfile = "nofile", "wipe", false
-    if autoopen and #o == 1 then vim.cmd("edit " .. vim.fn.fnameescape(o[1])) end
     if qf then vim.cmd("cgetbuffer | bd | copen | cc") end end
 vim.keymap.set("n", "<space>l", ":ls<cr>:b ")
 vim.keymap.set("n", "<space>j", function() vim.cmd("edit " .. vim.b._reg_dir) end)
 vim.keymap.set('n', '<space>y', function() vim.fn.setreg('+', vim.fn.expand('%:p')) end)
 vim.keymap.set("n", "<space>c", function() vim.ui.input({}, function(c) if c then ext(c) end end) end)
-vim.keymap.set("n", "<space>b", function() vim.ui.input({}, function(p) if p then
-    ext("grep -in '" .. p .. "' " .. vim.fn.shellescape(vim.fn.bufname("%"))) end end) end)
 vim.keymap.set("n", "<space>g", function() vim.ui.input({}, function(p) if p then 
-    ext(string.format("grep -IEnr %s '%s' %s", vim.b._g_excs, p, vim.b._sp), true, false, true) end end) end)
+    ext(string.format("grep -IEnr %s '%s' %s", vim.b._g_ex, p, vim.b._sp), true, true) end end) end)
 vim.keymap.set("n", "<space>f", function() vim.ui.input({}, function(p) if p then 
-    ext(string.format("find %s %s -path '*%s*' -print ", vim.b._sp, vim.b._f_excs, p), true, true) end end) end)
+    ext(string.format("find %s %s -path '*%s*' -print ", vim.b._sp, vim.b._f_ex, p), true) end end) end)
