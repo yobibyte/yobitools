4709711adc567144bf5b7de58bb266e7834cdfdc
yobibyte <y0b1byte@proton.me>
2025-06-28 22:06:39 +0100

    simplify

--- dotfiles/init.lua
+++ dotfiles/init.lua
@@ -3,6 +3,8 @@ vim.o.clipboard   = "unnamedplus"
 vim.opt.expandtab = true
 vim.cmd("syntax off | colorscheme retrobox | highlight Normal guifg=#ffaf00 guibg=#282828")
 
+local pdir = vim.fn.system("python3 -c 'import site; print(site.getsitepackages()[0])'"):gsub("%s+$", "") 
+local rdir = vim.fn.system("echo ${CARGO_HOME:-$HOME/.cargo}/registry/src/")
 function _G.t(n) vim.opt_local.shiftwidth, vim.opt_local.tabstop, vim.opt_local.softtabstop = n, n, n end
 local function ext(c, qf)
     o = vim.fn.systemlist(c) 
@@ -14,16 +16,14 @@ local function ext(c, qf)
 
 vim.api.nvim_create_autocmd("TextYankPost", { callback = function() vim.highlight.on_yank() end})
 vim.api.nvim_create_autocmd({"BufEnter", "FileType"}, { callback = function() t(4)
-    vim.b._reg_dir = vim.fn.system("python3 -c 'import site; print(site.getsitepackages()[0])'"):gsub("%s+$", "") 
-    if vim.bo.filetype == "rust" then
-        vim.b._reg_dir = vim.fn.system("echo ${CARGO_HOME:-$HOME/.cargo}/registry/src/") end
     local excs = { ".git", "*.egg-info", "__pycache__", "wandb", "target", ".venv" }
     vim.b._f_ex = table.concat(vim.tbl_map(function(e) return "-path '*" .. e .. "*' -prune -o" end, excs), " ")
     vim.b._g_ex = table.concat(vim.tbl_map(function(e) return "--exclude-dir='" .. e .. "'" end, excs), " ")
     vim.treesitter.stop() end})
 
 vim.keymap.set("n", "<space>l", ":ls<cr>:b ")
-vim.keymap.set("n", "<space>j", function() vim.fn.setreg('+', vim.b._reg_dir) end)
+vim.keymap.set("n", "<space>r", function() vim.fn.setreg('+', rdir) end)
+vim.keymap.set("n", "<space>p", function() vim.fn.setreg('+', pdir) end)
 vim.keymap.set('n', '<space>y', function() vim.fn.setreg('+', vim.fn.expand('%:p')) end)
 vim.keymap.set("n", "<space>c", function() vim.ui.input({}, function(c) if c then ext(c) end end) end)
 vim.keymap.set("n", "<space>g", function() vim.ui.input({}, function(p) if p then 
