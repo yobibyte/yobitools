6e7eca3c33e6fa7a5a8270c55c9933aa5ce50473
yobibyte <y0b1byte@proton.me>
2025-06-28 23:07:42 +0100

    simplify

--- dotfiles/init.lua
+++ dotfiles/init.lua
@@ -2,8 +2,13 @@ vim.o.undofile    = true
 vim.o.clipboard   = "unnamedplus"
 vim.opt.expandtab = true
 vim.cmd("syntax off | colorscheme retrobox | highlight Normal guifg=#ffaf00 guibg=#282828")
+local excs = { ".git", "*.egg-info", "__pycache__", "wandb", "target", ".venv" }
 
-function _G.t(n) vim.opt_local.shiftwidth, vim.opt_local.tabstop, vim.opt_local.softtabstop = n, n, n end
+function _G.t(n) 
+    vim.opt_local.shiftwidth = n
+    vim.opt_local.tabstop = n 
+    vim.opt_local.softtabstop = n end
+
 local function ext(c, qf)
     o = vim.fn.systemlist(c) 
     if not (o and #o > 0) then return end
@@ -12,12 +17,8 @@ local function ext(c, qf)
     vim.bo.buftype, vim.bo.bufhidden, vim.bo.swapfile = "nofile", "wipe", false
     if qf then vim.cmd("cgetbuffer | bd | copen | cc") end end
 
-vim.api.nvim_create_autocmd("TextYankPost", { callback = function() vim.highlight.on_yank() end})
-vim.api.nvim_create_autocmd({"BufEnter", "FileType"}, { callback = function() t(4)
-    local excs = { ".git", "*.egg-info", "__pycache__", "wandb", "target", ".venv" }
-    vim.b._f_ex = table.concat(vim.tbl_map(function(e) return "-path '*" .. e .. "*' -prune -o" end, excs), " ")
-    vim.b._g_ex = table.concat(vim.tbl_map(function(e) return "--exclude-dir='" .. e .. "'" end, excs), " ")
-    vim.treesitter.stop() end})
+vim.api.nvim_create_autocmd("TextYankPost", { callback = function() vim.highlight.on_yank()    end})
+vim.api.nvim_create_autocmd("BufEnter",     { callback = function() vim.treesitter.stop() t(4) end})
 
 vim.keymap.set("n", "<space>l", ":ls<cr>:b ")
 vim.keymap.set("n", "<space>r", function()
@@ -27,6 +28,8 @@ vim.keymap.set("n", "<space>p", function() vim.fn.setreg('+',
 vim.keymap.set('n', '<space>y', function() vim.fn.setreg('+', vim.fn.expand('%:p')) end)
 vim.keymap.set("n", "<space>c", function() vim.ui.input({}, function(c) if c then ext(c) end end) end)
 vim.keymap.set("n", "<space>g", function() vim.ui.input({}, function(p) if p then 
-    ext(string.format("grep -IEnr %s '%s' %s", vim.b._g_ex, p, vim.fn.getcwd()), true) end end) end)
+    local ex = table.concat(vim.tbl_map(function(e) return "--exclude-dir='" .. e .. "'" end, excs), " ")
+    ext(string.format("grep -IEnr %s '%s' %s", ex, p, vim.fn.getcwd()), true) end end) end)
 vim.keymap.set("n", "<space>f", function() vim.ui.input({}, function(p) if p then 
-    ext(string.format("find %s %s -path '*%s*' -print ", vim.fn.getcwd(), vim.b._f_ex, p)) end end) end)
+    local ex = table.concat(vim.tbl_map(function(e) return "-path '*" .. e .. "*' -prune -o" end, excs), " ")
+    ext(string.format("find %s %s -path '*%s*' -print ", vim.fn.getcwd(), ex, p)) end end) end)
