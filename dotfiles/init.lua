7e5c4d6fd075c4b398cf8589e92207bf972a37e5
yobibyte <y0b1byte@proton.me>
2025-07-03 19:48:47 +0100

    14

--- dotfiles/init.lua
+++ dotfiles/init.lua
@@ -1,8 +1,8 @@
-vim.o.undofile    = true
-vim.o.clipboard   = "unnamedplus"
-vim.opt.expandtab = true
+vim.o.undofile      = true
+vim.o.clipboard     = "unnamedplus"
+vim.opt.expandtab   = true
+vim.opt.shiftwidth  = 4
 vim.opt.softtabstop = -1
-vim.opt.shiftwidth = 4
 vim.cmd("syntax off | colorscheme retrobox | highlight Normal guifg=#ffaf00 guibg=#282828")
 vim.api.nvim_create_autocmd("TextYankPost", { callback = function() vim.highlight.on_yank() end})
 vim.api.nvim_create_autocmd("BufEnter",     { callback = function() vim.treesitter.stop() end})
@@ -10,9 +10,5 @@ vim.opt.grepprg = "rg --vimgrep --no-ignore-parent --glob='!wandb/**' -- $*"
 vim.keymap.set("n", "<space>l", ":ls<cr>:b ")
 vim.keymap.set('n', '<space>y', function() vim.fn.setreg('+', vim.fn.expand('%:p')) end)
 vim.keymap.set("n", "<space>c", function() vim.ui.input({}, function(c) if c then 
-    o = vim.fn.systemlist(c) if not (o and #o > 0) then return end
-    vim.cmd("vnew") 
-    vim.bo.buftype = "nofile"
-    vim.bo.bufhidden = "wipe" 
-    vim.bo.swapfile = false
-    vim.api.nvim_buf_set_lines(0, 0, -1, false, o) end end) end)
+  vim.cmd("vnew") vim.bo.buftype = "nofile" vim.bo.bufhidden = "wipe" vim.bo.swapfile = false
+  vim.api.nvim_buf_set_lines(0, 0, -1, false, vim.fn.systemlist(c)) end end) end)
