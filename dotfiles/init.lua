1b6c99a6156ad1108134c5ba616bac825b30d395
yobibyte <y0b1byte@proton.me>
2025-07-03 18:54:09 +0100

    lint

--- dotfiles/init.lua
+++ dotfiles/init.lua
@@ -4,7 +4,7 @@ vim.opt.expandtab = true
 vim.opt.softtabstop = -1
 vim.opt.shiftwidth = 4
 vim.cmd("syntax off | colorscheme retrobox | highlight Normal guifg=#ffaf00 guibg=#282828")
-vim.api.nvim_create_autocmd("TextYankPost", { callback = function() vim.highlight.on_yank()    end})
+vim.api.nvim_create_autocmd("TextYankPost", { callback = function() vim.highlight.on_yank() end})
 vim.api.nvim_create_autocmd("BufEnter",     { callback = function() vim.treesitter.stop() end})
 vim.opt.grepprg = "rg --vimgrep --no-ignore-parent --glob='!wandb/**' -- $*"
 vim.keymap.set("n", "<space>l", ":ls<cr>:b ")
