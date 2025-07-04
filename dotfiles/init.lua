d20ab92dc0b269acea47f3946b8c6f03cc1b441d
yobibyte <y0b1byte@proton.me>
2025-07-04 20:21:00 +0100

    simplify

--- dotfiles/init.lua
+++ dotfiles/init.lua
@@ -6,7 +6,6 @@ vim.opt.softtabstop = -1
 vim.cmd("syntax off | colorscheme retrobox | highlight Normal guifg=#ffaf00 guibg=#282828")
 vim.api.nvim_create_autocmd("TextYankPost", { callback = function() vim.highlight.on_yank() end})
 vim.api.nvim_create_autocmd("BufEnter",     { callback = function() vim.treesitter.stop() end})
-vim.opt.grepprg = "rg --vimgrep --no-ignore-parent -- $*"
 vim.keymap.set("n", "<space>l", ":ls<cr>:b ")
 vim.keymap.set('n', '<space>y', function() vim.fn.setreg('+', vim.fn.expand('%:p')) end)
 vim.keymap.set("n", "<space>c", function() vim.ui.input({}, function(c) if c then 
