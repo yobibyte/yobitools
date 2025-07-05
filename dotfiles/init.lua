b5a5365e9f7670454bec981c059ba3a3d7aa58fb
yobibyte <y0b1byte@proton.me>
2025-07-05 02:12:44 +0100

    simplify

--- dotfiles/init.lua
+++ dotfiles/init.lua
@@ -1,11 +1,10 @@
 vim.o.undofile      = true
 vim.o.clipboard     = "unnamedplus"
+vim.o.laststatus    = 0
 vim.opt.expandtab   = true
 vim.opt.shiftwidth  = 4
 vim.opt.softtabstop = -1
 vim.cmd("syntax off | colorscheme retrobox | highlight Normal guifg=#ffaf00 guibg=#282828")
-vim.api.nvim_create_autocmd("TextYankPost", { callback = function() vim.highlight.on_yank() end})
-vim.api.nvim_create_autocmd("BufEnter",     { callback = function() vim.treesitter.stop() end})
 vim.keymap.set("n", "<space>l", ":ls<cr>:b ")
 vim.keymap.set('n', '<space>y', function() vim.fn.setreg('+', vim.fn.expand('%:p')) end)
 vim.keymap.set("n", "<space>c", function() vim.ui.input({}, function(c) if c then 
