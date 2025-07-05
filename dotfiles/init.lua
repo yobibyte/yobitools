c0695590b47c93c89b4967cc747745c369fd2964
yobibyte <y0b1byte@proton.me>
2025-07-05 09:17:58 +0100

    fix

--- dotfiles/init.lua
+++ dotfiles/init.lua
@@ -5,8 +5,7 @@ vim.opt.expandtab   = true
 vim.opt.shiftwidth  = 4
 vim.opt.softtabstop = -1
 vim.cmd("syntax off | colorscheme retrobox | highlight Normal guifg=#ffaf00 guibg=#282828")
-vim.keymap.set("n", "<space>l", ":ls<cr>:b ")
 vim.keymap.set('n', '<space>y', function() vim.fn.setreg('+', vim.fn.expand('%:p')) end)
-vim.keymap.set("n", "<space>c", function() vim.ui.input({}, function(c) if c then 
+vim.keymap.set("n", "<space>c", function() vim.ui.input({}, function(c) if c and c~="" then 
   vim.cmd("vnew") vim.bo.buftype = "nofile" vim.bo.bufhidden = "wipe" vim.bo.swapfile = false
   vim.api.nvim_buf_set_lines(0, 0, -1, false, vim.fn.systemlist(c)) end end) end)
