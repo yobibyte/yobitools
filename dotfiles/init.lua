86b738a106fe608473aedab2c04999a56b34bf7b
yobibyte <y0b1byte@proton.me>
2025-07-05 12:39:22 +0100

    simplify

--- dotfiles/init.lua
+++ dotfiles/init.lua
@@ -4,6 +4,6 @@ vim.o.laststatus    = 0
 vim.opt.expandtab   = true
 vim.opt.shiftwidth  = 4
 vim.opt.softtabstop = -1
-vim.cmd("syntax off | colorscheme retrobox | highlight Normal guifg=#ffaf00 guibg=#282828")
+vim.cmd("syntax off | highlight Normal guifg=#ffaf00 guibg=#282828")
 vim.keymap.set('n', '<space>y', function() vim.fn.setreg('+', vim.fn.expand('%:p')) end)
 vim.keymap.set("n", "<space>c", function() vim.cmd("noswapfile enew | setlocal buftype=nofile bufhidden=wipe | call feedkeys(':read !', 'n')") end)
