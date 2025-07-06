38e3ca8c83528d7003932bde34e29924746201e5
yobibyte <y0b1byte@proton.me>
2025-07-06 15:31:07 +0100

    turn off syntax highlighting for lua

--- dotfiles/init.lua
+++ dotfiles/init.lua
@@ -5,5 +5,6 @@ vim.opt.expandtab   = true
 vim.opt.shiftwidth  = 4
 vim.opt.softtabstop = -1
 vim.cmd("syntax off | highlight Normal guifg=#ffaf00 guibg=#282828")
+vim.api.nvim_create_autocmd("BufEnter", {callback = function() vim.treesitter.stop() end})
 vim.keymap.set('n', '<space>y', function() vim.fn.setreg('+', vim.fn.expand('%:p')) end)
 vim.keymap.set("n", "<space>c", function() vim.cmd("noswapfile enew | setlocal buftype=nofile bufhidden=wipe | call feedkeys(':r !', 'n')") end)
